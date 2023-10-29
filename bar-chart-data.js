95b7414c26e11ecbfd2bee352d50b9d3eeea5448
das399igste <87725508+das399igste@users.noreply.github.com>
2023-10-29 19:25:05 +0100

    Update bar-chart-data.js
    prototype getWeek() added. Fixed generation of labels for weeks.

--- bar-chart-data.js
+++ bar-chart-data.js
@@ -28,6 +28,18 @@ module.exports = function(RED) {
   RED.nodes.registerType("bar-chart-data2", barChartDataNode);
 }
 
+Date.prototype.getWeek = function () {
+    var date = new Date(this.getTime());
+    date.setHours(0, 0, 0, 0);
+    // Thursday in current week decides the year.
+    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+    // January 4 is always in week 1.
+    var week1 = new Date(date.getFullYear(), 0, 4);
+    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
+        - 3 + (week1.getDay() + 6) % 7) / 7);
+}
+
 function setMeterValue(msg, myNode, store) {
 	if(msg.topic == '') {
 		myNode.error('topic is missing, cannot set the new meter value');
@@ -253,9 +265,7 @@ function barChartData(msg,myNode, store) {
 			return fullKey.slice(0, -6);
 		}
 		else if (myNode.x_interval == "weeks") {
-			var ISOWeekday = (0 == date.getDay()) ? 7 : date.getDay();
-			var ISOCalendarWeek = Math.floor((((date.getTime() - (new Date(date.getFullYear(), 0, 1)).getTime()) / 86400000) - ISOWeekday + 10) / 7);
-			return fullKey.slice(0, -10) + ISOCalendarWeek;
+			return fullKey.slice(0, -10) + date.getWeek();
 		}
 		else if (myNode.x_interval == "months") {
 			return fullKey.slice(0, -8);
@@ -321,9 +331,7 @@ function barChartData(msg,myNode, store) {
 				label = ("0" + (date.getMonth()+1)).slice(-2) + "-" + ("0" + date.getDate()).slice(-2);
 			}
 			else if (myNode.x_interval == "weeks") {
-				var ISOWeekday = (0 == date.getDay()) ? 7 : date.getDay();
-				var ISOCalendarWeek = Math.floor((((date.getTime() - (new Date(date.getFullYear(), 0, 1)).getTime()) / 86400000) - ISOWeekday + 10) / 7);
-				label = "" + date.getFullYear() + "-" + ("0" + ISOCalendarWeek).slice(-2);
+				label = "" + date.getFullYear() + "-" + ("0" + date.getWeek()).slice(-2);
 			}
 			else if (myNode.x_interval == "months") {
 				label = "" + date.getFullYear() + "-" + ("0" + (date.getMonth()+1)).slice(-2);
